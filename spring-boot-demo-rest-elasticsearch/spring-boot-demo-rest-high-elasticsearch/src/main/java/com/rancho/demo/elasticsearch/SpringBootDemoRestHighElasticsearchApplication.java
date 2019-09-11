9e615357cf0ab7b487758b295ec050c81a1263dc
yamt1002 <jinnan.yang@androidmov.com>
2019-09-11 15:02:47 +0800

    上传springboot-demo-elasticsearch源码

---
+++ spring-boot-demo-rest-elasticsearch/spring-boot-demo-rest-high-elasticsearch/src/main/java/com/rancho/demo/elasticsearch/SpringBootDemoRestHighElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.rancho.demo.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoRestHighElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoRestHighElasticsearchApplication.class, args);
+    }
+
+}
